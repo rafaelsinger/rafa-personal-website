44ea490a5266f43c1a318ee6d236919e2dd9a085
Rafael Singer <rafaelsinger66@gmail.com>
2022-07-17 11:34:15 -0700

    fixed 100vh mobile bug and image formatting on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Portfolio />} />
+        <Route path='/*' element={<Portfolio />} />
         <Route path='reviews'>
           <Route path='' element={<Navigate to='home' />} />
           <Route path='home' element={<BlogLandingPage />} />
