2e1cbc01e2600c8acc79867954115c3b9ddc2275
Rafael Singer <rafaelsinger66@gmail.com>
2022-08-06 01:27:28 -0700

    nearly finished landing page for both mobile and desktop, fixed overall layout

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,5 +1,9 @@
 import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import {RiGithubLine} from 'react-icons/ri'
+import {GrLinkedinOption} from 'react-icons/gr'
+import {IoMdDocument} from 'react-icons/io'
+
 
 
 const Portfolio = () => {
@@ -10,7 +14,14 @@ const Portfolio = () => {
   /* <Link to='/reviews'><h2 className='reviews-link'>Go to Reviews</h2></Link> */
   return (
     <div className='wrapper'>
+      <div className='main-circle'></div>
+      <div className='small-circle'></div>
       <div className='col-white'></div>
+        <div className='icon-container'>
+          <i className='icon' id='github'><RiGithubLine size={50} /></i>
+          <i className='icon' id='linkedin'><GrLinkedinOption size={50} /></i>
+          <i className='icon' id='resume'><IoMdDocument size={50} /></i>
+        </div>
       <div className='portfolio-container'>
         <h1 className='name'>Rafael Singer</h1>
         <h2 className='position'>Fullstack Web Developer</h2>
@@ -24,6 +35,15 @@ const Portfolio = () => {
       </div>
       <section id='projects'>
           projects here
+      </section>
+      <section id='skills'>
+          skills here
+      </section>
+      <section id='about'>
+          about here
+      </section>
+      <section id='contact'>
+          contact here
       </section>
     </div>
   )
